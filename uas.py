419b367bc567d85e53ec1dc0eea7779341b8ec36
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-11-30 20:23:16 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -31,8 +31,6 @@ with upload_data:
     uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
     for uploaded_file in uploaded_files:
         df = pd.read_csv( uploaded_file.name)
-        st.write("Nama File Anda = ", data.name)
-        st.dataframe(df)
 
 with preporcessing:
     st.write("""# Preprocessing""")
