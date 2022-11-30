7daac34980a30cfaa5b3c34f8a880496abb646ba
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-12-01 06:56:08 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -4,8 +4,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 import streamlit as st
@@ -55,7 +54,6 @@ with preporcessing:
     y_pred_rf= model.predict(X_test)
     y_pred_rf
     
-    from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
     cm = confusion_matrix(y_test,y_pred_rf)
     print(cm)
     
