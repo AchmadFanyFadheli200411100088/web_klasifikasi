f450e6315dae153a14b1871b5a7c5c1ac53f0d70
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-12-01 04:59:51 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -55,6 +55,17 @@ with preporcessing:
     scaler = MinMaxScaler()
     scaled = scaler.fit_transform(X)
     st.write("Hasil Preprocesing : ", scaled)
+    
+    "### Splitting the dataset into training and testing data"
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 0)
+    st.write("Shape for training data", X_train.shape, y_train.shape)
+    st.write("Shape for testing data", X_test.shape, y_test.shape)
+
+    "### Feature Scaling"
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    X_train,X_test
 
 with modeling:
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
