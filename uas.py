860ef13e0663eb294dd859ffaa3d764d8b043a2e
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-12-01 05:08:45 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 import streamlit as st
 
@@ -62,14 +63,14 @@ with preporcessing:
     st.write("Shape for testing data", X_test.shape, y_test.shape)
 
     "### Feature Scaling"
-    scaler = StandardScaler()
+    score = accuracy_score()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     X_train,X_test
 
 with modeling:
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
-    sc = StandardScaler()
+    score = accuracy_score()
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)  
     st.write("""# Modeling """)
