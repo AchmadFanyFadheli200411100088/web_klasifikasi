a8d9fc2abf1e5da5d306da725a46d33ff49c9b52
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-12-01 07:21:48 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -72,7 +72,7 @@ with preporcessing:
     
 
 with modeling:
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state= 0)
     from sklearn.preprocessing import StandardScaler
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
