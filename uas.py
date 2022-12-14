054ed8406eb33736f422e0456a1eeccac35c8733
AchmadFanyFadheli200411100088 <112039042+AchmadFanyFadheli200411100088@users.noreply.github.com>
2022-12-14 16:38:39 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -41,9 +41,6 @@ with upload_data:
 with preporcessing:
     st.write("""# Preprocessing""")
 
-    X= df.drop(columns="Type")
-    y= df.Type
-
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
     X_train
     y_train
